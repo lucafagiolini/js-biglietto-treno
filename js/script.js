a05dc04a711ae6c6a0fec794c330c639332c1984
Luca <lucafagiolini@live.it>
2024-01-26 17:21:47 +0100

    macro struttura

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+let kmAmmount = prompt('inserisci qui i KM da percorrere')
+console.log(kmAmmount)
+
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+let age = prompt('inserisci qui la tua età')
+console.log(age)
+
+// il prezzo del biglietto è definito in base ai km (0.21 € al km)
+
+// va applicato uno sconto del 20% per i minorenni
+
+// va applicato uno sconto del 40% per gli over 65.
+
+// L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+
+
+
+
+
+
+
