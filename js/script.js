7422634e678a7ab09c1ae7c2ade91f0d50b720fc
Luca <lucafagiolini@live.it>
2024-01-26 17:31:54 +0100

    discount added

--- js/script.js
+++ js/script.js
@@ -11,11 +11,16 @@ let price = kmAmmount * 0.21
 console.log(price)
 
 // va applicato uno sconto del 20% per i minorenni
+let discount20 = price - (price * 0.2)
+console.log(discount20)
 
 // va applicato uno sconto del 40% per gli over 65.
+let discount40 = price - (price * 0.4)
+console.log(discount40)
+
+
 
 // L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
-
 
 
 
